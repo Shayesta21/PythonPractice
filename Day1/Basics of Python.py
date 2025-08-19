c900afad86ff88e091973fd5791c4bfe49ca35ac
Shayesta <nausheenshayesta21@gmail.com>
2025-08-19 16:40:48 -0400

    Learning comments in python

--- Day1/Basics of Python.py
+++ Day1/Basics of Python.py
@@ -1,1 +1,7 @@
+#Learning Comments in Python
+# is used for a single line comment
 print("hello world")
+#for multiple line comments use 3(""") in the beginning and in the end
+""" 
+these  are multiple line comments
+"""
